61aba30c6da995669134df3d8d538aea746e9749
ROBOTICS-3D.com <123720940+marrtino@users.noreply.github.com>
2025-03-10 15:42:52 +0100

    Update facestart.sh

--- facestart.sh
+++ facestart.sh
@@ -2,4 +2,4 @@
 # echo "Launch Browser"
 #DISPLAY=:0 midori -p -e Fullscreen -a http://localhost:8080/social/marrtina.html 
 rm /home/marrtino/snap/chromium/common/chromium/SingletonLock 
-chromium-browser --app=http://localhost/social/marrtina07.html   -start-maximized
+chromium-browser --app=http://localhost/social/marrtina07.html   --kiosk
