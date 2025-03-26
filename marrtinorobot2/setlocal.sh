6d15190117d1af3e9321b192732a4f77632b7efe
Fabio Ferrarini <ferrarini09@gmail.com>
2025-03-26 19:53:19 +0100

    Vers.1.1

--- marrtinorobot2/setlocal.sh
+++ marrtinorobot2/setlocal.sh
@@ -1,7 +1,4 @@
-sudo chown -R www-data:www-data $HOME/src/marrtinorobot2/marrtinorobot2_webinterface/www
-sudo chmod 0666 $MARRTINOROBOT2_WEBI/www/bringup/commands.json
-sudo chmod 0666 $MARRTINOROBOT2_WEBI/www/social/tmp/facerobot.txt
-sudo chmod 0666 $MARRTINOROBOT2_WEBI/www/viewer/img/lastimage.jpg
-sudo chmod 777 -R $HOME/src/marrtinorobot2/marrtinorobot2_webinterface/www/tombola/ 
-sudo chmod 777 -R $HOME/src/marrtinorobot2/marrtinorobot2_webinterface/www/quiz/js/questions.js
+# ripristina permessi
+sudo chown -R $USER:$USER $HOME/src/marrtinorobot2/marrtinorobot2_webinterface/www
+
 
